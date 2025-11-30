eb2d9a7d1af1391c9a154053ba51de5b5d060b94
manimozhiyan23 <manimozhiyan88@gmail.com>
2025-11-30 11:35:40 +0530

    backend process

--- frontend/src/pages/customerLogin.js
+++ frontend/src/pages/customerLogin.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./../styles/auth.css";
-import BackButton from "../components/backButton";
+import BackButton from "../components/BackButton.js";
 
 function CustomerLogin() {
   return (
